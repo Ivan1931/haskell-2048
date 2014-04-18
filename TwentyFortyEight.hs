581e98e76156708ecaf5b866c204e2f19e49209f
jonah-hooper <jonah.graham.hooper@gmail.com>
2014-04-18 19:55:09 +0200

    Wrote merge. Did some refactoring. Wrote full transision. All tests pass.

--- TwentyFortyEight.hs
+++ TwentyFortyEight.hs
@@ -2,15 +2,20 @@ module TwentyFortyEight where
 
 import qualified Data.Map as Map
 import qualified Data.List as List
+import Control.Applicative
 import Data.Maybe
 
 top = 3
+two = Just (2 :: Int)
+four = Just (4 :: Int)
 
 type X = Int
 type Y = Int
 type Coord = (X, Y)
 type Square = Maybe Int
 type Board = Map.Map Coord Square
+type Operation = (Board -> Coord -> Board)
+type Predicate = (Board -> Coord -> Bool)
 
 toString :: Board -> String
 toString _ = undefined
@@ -29,18 +34,14 @@ setCoords :: Board -> [(Coord, Square)] -> Board
 setCoords board list = List.foldl' step board list
   where step board' xy = set board' xy
 
---Warning, this will overide adjasent blocks. Use with canShiftSquare
-shiftSquare :: Board -> Coord -> Board
-shiftSquare board xy@(x,y) 
+applyToSquare :: Board -> Operation -> Predicate -> Coord -> Board
+applyToSquare board op pred xy@(x,y) 
   | x == top          = board
   | isNothing current = board
-  | cantShift         = board
-  | otherwise         = blankLast $ shiftToNext
+  | cant              = board
+  | otherwise         = op board xy
   where current     = fromJust $ Map.lookup xy board
-        cantShift   = not $ canShiftSquare board xy 
-        next        = (x + 1, y)
-        shiftToNext = Map.adjust (\_ -> current) next board
-        blankLast   = Map.adjust (\_ -> Nothing) xy
+        cant        = not $ pred board xy 
 
 canShiftSquare :: Board -> Coord -> Bool
 canShiftSquare board (x,y)
@@ -48,18 +49,30 @@ canShiftSquare board (x,y)
   | isNothing nextSquare = True
   | otherwise            = False
   where nextSquare       = fromJust $ Map.lookup (x + 1,y) board
-                                       
-shiftColumn :: Board -> Board
-shiftColumn board = iterateShits board (3,3)
-  where iterateShits board' xy@(x,y) = let nextBoard = shiftSquare board' xy
+
+shiftSquare :: Board -> Coord -> Board
+shiftSquare board xy = applyToSquare board shiftToNext canShiftSquare xy
+  where shiftToNext board' xy'@(x,y) = let next        = (x + 1, y)
+                                           current     = fromJust $ Map.lookup xy board'
+                                           shiftToNext = Map.adjust (\_ -> current) next board'
+                                           blankLast   = Map.adjust (\_ -> Nothing) xy'
+                                        in blankLast $ shiftToNext
+
+applyToAll :: Board -> Operation -> Board
+applyToAll board op = iterate board (3,3)
+  where iterate board' xy@(x,y) = let nextBoard = op board' xy
                                        in case xy of
                                          (0,0) -> nextBoard
-                                         (0,_) -> iterateShits nextBoard (3, y - 1)
-                                         (_,_) -> iterateShits nextBoard (x - 1, y)
+                                         (0,_) -> iterate nextBoard (3, y - 1)
+                                         (_,_) -> iterate nextBoard (x - 1, y)
+                                       
+--performs shift on each square
+shift :: Board -> Board
+shift board = applyToAll board shiftSquare
 
---hehehehehehe
+--performs shift on each square four times
 fullShift :: Board -> Board
-fullShift = shiftColumn . shiftColumn . shiftColumn . shiftColumn
+fullShift = shift . shift . shift . shift
 
 
 canMergeSquare :: Board -> Coord -> Bool
@@ -68,8 +81,20 @@ canMergeSquare board xy@(x,y) = (current == next) && (not $ isNothing next) && (
           current = getSquare xy
           next = getSquare (x + 1, y)
 
+mergeSquare :: Board -> Coord -> Board
+mergeSquare board xy = applyToSquare board doMerge canMergeSquare xy
+  where doMerge board' xy'@(x,y) = let xy'' = (x + 1, y)
+                                       next = fromJust $ Map.lookup xy'' board'
+                                       current = fromJust $ Map.lookup xy' board' 
+                                       mergeNext = Map.adjust (\a -> (fmap (+) next) <*> a) xy'' board'
+                                       blankLast = Map.adjust (\_ -> Nothing) xy' 
+                                   in blankLast $ mergeNext
+
 merge :: Board -> Board
-merge _ = undefined
+merge board = applyToAll board mergeSquare 
+
+transition :: Board -> Board
+transition board = fullShift $ merge $ fullShift board
 
 rotate :: Board -> Board
 rotate _ = undefined
--- TwentyFortyEight_test.hs
+++ TwentyFortyEight_test.hs
@@ -7,77 +7,98 @@ empty = fromString "****\n\
                    \****\n\
                    \****"
 
-main = hspec $ do
-  let lineAtFront = setCoords empty [((0,j), Just 2) | j <- [0 .. top]]
-      lineAtEnd   = setCoords empty [((3,j), Just 2) | j <- [0 .. top]]
-      lineAtSecond   = setCoords empty [((1,j), Just 2) | j <- [0 .. top]]
-      --Made the subsequent indexes different to observe whether the
-      --shifts preserve order
-      twoRowsAtFront = setCoords empty [((i,j), Just (2 * i)) | i <- [0,1], j <- [0 .. top]]
-      twoRowsAtSecond = setCoords empty [((i,j), Just (2 * (i - 1))) | i <- [1,2], j <- [0 .. top]] 
-      twoRowsAtEnd = setCoords empty [((i,j), Just (2 * (i - 2))) | i <- [2,3], j <- [0 .. top]] 
-      atStart = set empty ((0,0), Just 2)
-      atEnd   = set empty ((3,0), Just 2)
-      atSecondPosition = set empty ((1,0), Just 2)
-      twoInRow = set atStart ((1,0), Just 2)
-      initial = set empty ((0,0), Just 2)
-      afterShift = set empty ((3,0), Just 2)
+start = (0,0)
+end = (3,0)
+lineAtFront = setCoords empty [((0,j), Just 2) | j <- [0 .. top]]
+lineAtEnd   = setCoords empty [((3,j), Just 2) | j <- [0 .. top]]
+lineAtSecond   = setCoords empty [((1,j), Just 2) | j <- [0 .. top]]
+--Made the subsequent indexes different to observe whether the
+--shifts preserve order
+twoRowsAtFront = setCoords empty [((i,j), Just (2 * i)) | i <- [0,1], j <- [0 .. top]]
+twoRowsAtSecond = setCoords empty [((i,j), Just (2 * (i - 1))) | i <- [1,2], j <- [0 .. top]] 
+twoRowsAtEnd = setCoords empty [((i,j), Just (2 * (i - 2))) | i <- [2,3], j <- [0 .. top]] 
+atStart = set empty (start, Just 2)
+atEnd   = set empty ((3,0), Just 2)
+atSecondPosition = set empty ((1,0), Just 2)
+twoInRow = set atStart ((1,0), Just 2)
+initial = set empty (start, Just 2)
+afterShift = set empty ((3,0), Just 2)
+mergedTwoInRow = set empty ((1,0), four)
+fullOfTwos = setCoords empty [((i, j), two) | i <- [0 .. top], j <- [0 .. top]]
+twoFourLines = setCoords empty [((i,j), four) | i <- [1, 3], j <- [0 .. top]]
+twoFourLinesAtEnd = setCoords empty [((i,j), four) | i <- [2, 3], j <- [0 .. top]]
 
+shifting = do
   describe "Shifting board to the right" $ do
     describe "Shifting one unit to the right" $ do
-
       describe "canShift" $ do
           it "Is true when there are no blocks to the right" $ do
-            canShiftSquare atStart (0,0)`shouldBe` True
-
+            canShiftSquare atStart start`shouldBe` True
           it "Is false when at the end" $ do
             canShiftSquare atEnd (3,0) `shouldBe` False
-
           it "Is false when a square follows immediately to the right" $ do
-            canShiftSquare twoInRow (0,0) `shouldBe` False
-
+            canShiftSquare twoInRow start `shouldBe` False
       describe "canShiftSquare" $ do
         it "Shifts one unit to the right when empty" $ do
-          shiftSquare atStart (0,0) `shouldBe` atSecondPosition
-
+          shiftSquare atStart start `shouldBe` atSecondPosition
         it "Does not shift the end square" $ do 
           shiftSquare atEnd (3,0) `shouldBe` atEnd
-        
         it "Does not shift if it means overriding a block" $ do
-          shiftSquare twoInRow (0,0) `shouldBe` twoInRow
-      
+          shiftSquare twoInRow start `shouldBe` twoInRow
     describe "board level shifts" $ do
-      describe "Single shiftColumn" $ do
-        it "shifts those in front to second" $ do
-          shiftColumn lineAtFront `shouldBe` lineAtSecond
+      describe "Single shift" $ do
+        it "shifts shift in front to second" $ do
+          shift lineAtFront `shouldBe` lineAtSecond
         it "does not change those at end" $ do
-          shiftColumn lineAtEnd `shouldBe` lineAtEnd
+          shift lineAtEnd `shouldBe` lineAtEnd
         it "shifts two adjacent rows one space to the left" $ do
-          shiftColumn twoRowsAtFront `shouldBe` twoRowsAtSecond
-
+          shift twoRowsAtFront `shouldBe` twoRowsAtSecond
       describe "when shifting entire board to the right" $ do
         it "shifts anything in the first position to the end" $ do
           fullShift initial `shouldBe` afterShift
-        
         it "shifts an entire row to the end" $ do
           fullShift lineAtFront `shouldBe` lineAtEnd
-
         it "shifts of the two adjacent rows at different starting indexes have same result" $ do
           fullShift twoRowsAtFront `shouldBe` fullShift twoRowsAtSecond
-
         it "shifts the first row and makes it look like the one at the end" $ do
           fullShift twoRowsAtFront `shouldBe` twoRowsAtEnd 
-
         it "shifts the two rows in starting at second x index and returns board where all rows are at end" $ do
           fullShift twoRowsAtSecond `shouldBe` twoRowsAtEnd
-  
+
+merging = do
   describe "Merging" $ do
     describe "canMerge" $ do
       it "with equal squares is true" $ do
-        canMergeSquare twoInRow (0,0) `shouldBe` True
+        canMergeSquare twoInRow start `shouldBe` True
       it "with empty and full square is false" $ do
-        canMergeSquare atStart (0,0) `shouldBe` False
+        canMergeSquare atStart start `shouldBe` False
       it "with two empty blocks is false" $ do
-        canMergeSquare empty (0,0) `shouldBe` False
+        canMergeSquare empty start `shouldBe` False
       it "with two adjacent unequal blocks cannot be merged" $ do
-        canMergeSquare (set twoInRow ((0,0), Just 4)) (0,0) `shouldBe` False 
+        canMergeSquare (set twoInRow (start, Just 4)) start `shouldBe` False 
+    describe "mergeSquare" $ do
+      it "merges equal squares into one square with double value of origional and empty adjacent squares" $ do
+        mergeSquare twoInRow start `shouldBe` mergedTwoInRow
+      it "does not merge non-equal squares leaving the board the same" $ do
+        mergeSquare twoRowsAtFront start `shouldBe` twoRowsAtFront 
+      it "does not break when merging nothing" $ do
+        mergeSquare empty start `shouldBe` empty
+      it "does not merge with empty squares" $ do
+        mergeSquare atStart start `shouldBe` atStart
+      it "does not merge square at end" $ do 
+        mergeSquare atEnd end `shouldBe` atEnd
+    describe "merge" $ do
+      it "merges a board full of twos into one that contains two line of fours at indexes 1, 3" $ do
+        merge fullOfTwos `shouldBe` twoFourLines
+
+fullTransition = do
+    describe "fullLeftTransision" $ do
+      it "merges a board full of twos to one with two rows of fours at the end" $ do
+        transition fullOfTwos `shouldBe` twoFourLinesAtEnd
+
+
+main = hspec $ do
+  shifting
+  merging
+  fullTransition
+
